edb309161b1e103af62aa9e227ab1942d4238076
Cezara Borsan <cezara@Cezaras-Air.home>
2024-03-21 20:48:00 +0100

    update flower section

--- src/components/Layout/FlowerSection.js
+++ src/components/Layout/FlowerSection.js
@@ -17,36 +17,28 @@ function FlowerSection() {
     return (
         <>
             <section className="flower-section" ref={targetRef}>
-                <h2 className="flower-heading">Discover New Handmade Items</h2>
                 <div class="flower-content">
-                    <h3 className="flower-heading ">Resin Flower Preservation</h3>
                     <div class="flower-grid">
                         <div class="flower-info">
+                            <h3 class="flower-heading"> <span class="section-label" >Discover New Handmade Items</span>Flowers Preservation</h3>
                             <div class="flower-info-content">
-                                <p class="flower-paragraph">As I start exploring new ways to express my creativity, I've begun making handmade treasures. One thing I'm really excited about is preserving flowers in resin.</p>
-                                <p class="flower-paragraph">In this collection, you'll discover coaster holders adorned with dried flowers that I've carefully chosen and preserved. These flowers have been selected for their beauty and uniqueness, and I've encased them in resin to capture their charm forever.</p>
+                                <p class="flower-paragraph">As I start exploring new ways to express my creativity, I've begun making handmade treasures. I found something I'm really excited about: preserving flowers in resin.</p>
+                                {/* <p class="flower-paragraph">In this collection, you'll discover coaster holders adorned with dried flowers that I've carefully chosen and preserved. These flowers have been selected for their beauty and uniqueness, and I've encased them in resin to capture their charm forever.</p> */}
 
                             </div>
-                            <LazyLoadedImage
-                                src="./images/flower-img3.jpeg"
-                                alt="fanny bag"
-                                className="about-image"
-                                loading="lazy"
-                            />
-
                         </div>
-                        <div>
-                            <LazyLoadedImage
-                                src="./images/flower-img1.jpeg"
-                                alt="fanny bag"
-                                className="about-image"
-                                loading="lazy"
-                            />
-
-                            <p class="flower-paragraph flower-paragraph-bigger"> Keep an eye out for thrilling updates as we further explore and enhance our collection together.</p>
-
-                        </div>
-
+                        <LazyLoadedImage
+                            src="./images/flower-img1.png"
+                            alt="fanny bag"
+                            className="flower-image flower-image-translate"
+                            loading="lazy"
+                        />
+                        <LazyLoadedImage
+                            src="./images/flower-img2.png"
+                            alt="fanny bag"
+                            className="flower-image"
+                            loading="lazy"
+                        />
                     </div>
                 </div>
             </section>
